88ecdcce5302e4ca66ca15e2946fcd46182ad7d4
jicheng.liu <jicheng.liu@chinascope.com>
2023-07-05 17:11:37 +0800

    两数之和

--- src/main/java/leetcode/editor/cn/[1]两数之和.java
+++ src/main/java/leetcode/editor/cn/[1]两数之和.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
+
     public int[] twoSum(int[] nums, int target) {
         Map<BigInteger,Integer> key = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
